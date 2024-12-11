dd4081e72809c15b4763847f0242842f212b71e3
kitoyaka <vlasiukdima007@gmail.com>
2024-12-11 12:24:48 +0200

    9 commit

---
+++ libConnect.h
@@ -0,0 +1,13 @@
+#ifndef  LIBCONNECT_H
+#define  LIBCONNECT_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <thread>
+#include <ctime>
+#include <nlohmann/json.hpp>
+#include <fstream>
+
+#endif LIBCONNECT_H
